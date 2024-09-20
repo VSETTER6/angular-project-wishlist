faad69d781b12df75666b51d5ffaa717e3675d30
VSETTER6 <vilhelm.settergren@live.se>
2024-09-20 14:19:16 +0200

    2.5

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { WishItem } from '../models/wishItem';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,10 @@ import { Component } from '@angular/core';
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'wishlist';
+  items : WishItem[] = [
+    new WishItem('Learn Angular'),
+    new WishItem('Get coffee', true),
+    new WishItem('Grass that cuts itself')
+  ]
+  title = 'my wishlist';
 }
