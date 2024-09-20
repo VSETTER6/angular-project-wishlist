2d3b8655f7794ec1e8a7fa1873c3540614014cbf
VSETTER6 <vilhelm.settergren@live.se>
2024-09-20 15:52:13 +0200

    2.8

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,18 +8,34 @@ import { WishItem } from '../models/wishItem';
 })
 export class AppComponent {
   items : WishItem[] = [
-    // new WishItem('Learn Angular'),
-    // new WishItem('Get coffee', true),
-    // new WishItem('Grass that cuts itself')
+    new WishItem('Learn Angular'),
+    new WishItem('Get coffee', true),
+    new WishItem('Grass that cuts itself')
   ]
+
+  listFilter : String = '0';
+
+  newWishText = "";
 
   title = 'wishlist';
 
-  newWishText = "";
+  visableItems : WishItem[] = this.items;
 
   addNewWish() {
     this.items.push(new WishItem(this.newWishText));
     this.newWishText = '';
+  }
+
+  filterChanged(value : any){
+    if(value == 0) {
+      this.visableItems = this.items;
+    }
+    else if (value == 1) {
+      this.visableItems = this.items.filter(item => !item.isCompleted);
+    }
+    else {
+      this.visableItems = this.items.filter(item => item.isCompleted);
+    }
   }
 
   toggleItem(item : WishItem) {
